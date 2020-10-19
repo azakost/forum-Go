694b0977ddd6abcafcd0819545662587ffc9a499
Azamat Alimbayev <azakost@gmail.com>
2020-10-19 12:54:25 +0600

    cleaned up initial queries

--- config.go
+++ config.go
@@ -23,28 +23,56 @@ CREATE TABLE users (
 	status INTEGER NOT NULL DEFAULT 123,
 	UNIQUE (username, email) );
 
-INSERT INTO users(email, username, password, fullname) 
-	values(
-		'azakost@gmail.com',
-		'azakost',
-		'$2a$04$kitMig4Sfj/Id0C85pysxu3MQbFMC0qXDn5j4RA8ZoI8P9GMcE8Vm',
-		'Azamat Alimbayev'
-	);
-
-INSERT INTO users(email, username, password, fullname) 
-	values(
-		'geradot@gmail.com',
-		'udot',
-		'$2a$04$kitMig4Sfj/Id0C85pysxu3MQbFMC0qXDn5j4RA8ZoI8P9GMcE8Vm',
-		'German Udotov'
-	);
-
 CREATE TABLE categories (
 	categoryId INTEGER PRIMARY KEY AUTOINCREMENT,
 	created DATETIME DEFAULT CURRENT_TIMESTAMP,
 	name TEXT NOT NULL,
 	description TEXT NOT NULL );
 
+CREATE TABLE posts (
+	postId INTEGER PRIMARY KEY AUTOINCREMENT,
+	posted DATETIME DEFAULT CURRENT_TIMESTAMP,
+	userId INTEGER NOT NULL,
+	title TEXT NOT NULL,
+	text TEXT NOT NULL,
+	categories TEXT NOT NULL,
+	status INTEGER DEFAULT 1 );
+
+CREATE TABLE postReactions (
+	reactionId INTEGER PRIMARY KEY AUTOINCREMENT,
+	reacted DATETIME DEFAULT CURRENT_TIMESTAMP,
+	postId INTEGER NOT NULL,
+	userId INTEGER NOT NULL,
+	reaction TEXT DEFAULT 'idle',
+	UNIQUE (userId, postId));
+
+CREATE TABLE comments (
+	commentId INTEGER PRIMARY KEY AUTOINCREMENT,
+	commented DATETIME DEFAULT CURRENT_TIMESTAMP,
+	postId INTEGER NOT NULL,
+	userId INTEGER NOT NULL,
+	comment TEXT NOT NULL );	
+
+CREATE TABLE commentReactions (
+	reactionId INTEGER PRIMARY KEY AUTOINCREMENT,
+	reacted DATETIME DEFAULT CURRENT_TIMESTAMP,
+	commentId INTEGER NOT NULL,
+	userId INTEGER NOT NULL,
+	reaction TEXT DEFAULT 'idle',
+	UNIQUE (userId, commentId));
+
+INSERT INTO users(email, username, password, fullname, role) 
+	values(
+		'azakost@gmail.com',
+		'azakost',
+		'$2a$04$kitMig4Sfj/Id0C85pysxu3MQbFMC0qXDn5j4RA8ZoI8P9GMcE8Vm',
+		'Azamat Alimbayev',
+		'admin'
+	);
+` +
+
+	// Testing queries - should be deleted after
+	`
 INSERT INTO categories(name, description)
 	values(
 		'golang',
@@ -55,16 +83,7 @@ INSERT INTO categories(name, description)
 	values(
 		'js',
 		'JavaScript is a mother of all web devs!'
-	);
-
-CREATE TABLE posts (
-	postId INTEGER PRIMARY KEY AUTOINCREMENT,
-	posted DATETIME DEFAULT CURRENT_TIMESTAMP,
-	userId INTEGER NOT NULL,
-	title TEXT NOT NULL,
-	text TEXT NOT NULL,
-	categories TEXT NOT NULL,
-	status INTEGER DEFAULT 1 );
+	);	
 
 INSERT INTO posts(userId, title, text, categories) 
 	values(
@@ -81,24 +100,6 @@ INSERT INTO posts(userId, title, text, categories)
 		'sddsd',
 		'"2"'
 	);
-		
-CREATE TABLE reactions (
-	reactionId INTEGER PRIMARY KEY AUTOINCREMENT,
-	reacted DATETIME DEFAULT CURRENT_TIMESTAMP,
-	postId INTEGER NOT NULL,
-	userId INTEGER NOT NULL,
-	reaction TEXT DEFAULT 'idle',
-	UNIQUE (userId, postId));
-
-INSERT INTO reactions(postId, userId, reaction) 
-	values('1',	'1', 'like');
-
-CREATE TABLE comments (
-	commentId INTEGER PRIMARY KEY AUTOINCREMENT,
-	commented DATETIME DEFAULT CURRENT_TIMESTAMP,
-	postId INTEGER NOT NULL,
-	userId INTEGER NOT NULL,
-	comment TEXT NOT NULL );	
 
 INSERT INTO comments(postId, userId, comment) 
 	values('1',	'2', 'Sad! Not for udots!');
@@ -109,12 +110,7 @@ INSERT INTO comments(postId, userId, comment)
 INSERT INTO comments(postId, userId, comment) 
 	values('2',	'1', 'Bester content!');
 
-CREATE TABLE comreact (
-	reactionId INTEGER PRIMARY KEY AUTOINCREMENT,
-	reacted DATETIME DEFAULT CURRENT_TIMESTAMP,
-	commentId INTEGER NOT NULL,
-	userId INTEGER NOT NULL,
-	reaction TEXT DEFAULT 'idle',
-	UNIQUE (userId, commentId));
+INSERT INTO reactions(postId, userId, reaction) 
+	values('1',	'1', 'like');
 
 `
